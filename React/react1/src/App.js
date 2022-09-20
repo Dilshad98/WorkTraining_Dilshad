9dedd5be82e353c66811eb835d930d6290bfd993
Dilshad98 <83205290+Dilshad98@users.noreply.github.com>
2022-09-20 17:11:33 +0800

    DailyAssignmentWeek3Day2

---
+++ React/react1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="outline">
+       <h1 className="h1tag">Welcome to React!!</h1>
+       <h2>My First React Application</h2>
+    </div>
+   
+  );
+}
+
+export default App;
